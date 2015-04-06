6b2ddfbfde16fb63f8b04d257511fb8c7f37c196
Jacob Cook <jacob@peakwinter.net>
2015-04-06 18:23:32 -0400

    bugfix

--- arkos/websites.py
+++ arkos/websites.py
@@ -452,7 +452,7 @@ class Site:
             "database": self.db.id if self.db else None,
             "php": self.php,
             "enabled": self.enabled,
-            "has_actions": self.meta.website_extra_actions if self.meta else None,
+            "has_actions": self.meta.website_extra_actions if hasattr(self.meta, "website_extra_actions") else None,
             "is_ready": True
         }
 
