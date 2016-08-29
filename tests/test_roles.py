ab3e39c9eec93de28dc0f8a12033c2b92bc93a28
Jacob Cook <jacob@peakwinter.net>
2016-08-29 18:52:37 -0400

    add group and domain tests

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -69,11 +69,7 @@ class RolesTestCase(unittest.TestCase):
         del self.ldapobj
 
     def test_add_user(self):
-        u = users.User(
-            name="testuser", first_name="Test", last_name="User",
-            domain="localhost", admin=True, sudo=False
-        )
-        u.add(passwd="testpass")
+        _add_test_user("testuser")
         u = users.get(name="testuser")
         self.assertIsNotNone(u)
         self.assertEqual(u.name, "testuser")
@@ -83,20 +79,76 @@ class RolesTestCase(unittest.TestCase):
         self.assertTrue(u.admin)
         self.assertFalse(u.sudo)
 
+    def test_upd_user(self):
+        _add_test_user("testuser")
+        u = users.get(name="testuser")
+        u.first_name = "Notatest"
+        u.last_name = ""
+        u.update(passwd="mypass")
+        u = users.get(name="testuser")
+        self.assertEqual(u.first_name, "Notatest")
+        self.assertEqual(u.last_name, "")
+        self.assertTrue(u.verify_passwd("mypass"))
+
+    def test_del_user(self):
+        _add_test_user("testuser")
+        u = users.get(name="testuser")
+        u.delete()
+        self.assertIsNone(users.get(name="testuser"))
+
     def test_auth_success(self):
-        u = users.User(
-            name="testuser", first_name="Test", last_name="User",
-            domain="localhost", admin=True, sudo=False
-        )
-        u.add(passwd="testpass")
+        _add_test_user("testuser")
         u = users.get(name="testuser")
         self.assertTrue(u.verify_passwd("testpass"))
 
     def test_auth_fail(self):
-        u = users.User(
-            name="testuser", first_name="Test", last_name="User",
-            domain="localhost", admin=False, sudo=False
-        )
-        u.add(passwd="testpass")
+        _add_test_user("testuser")
         u = users.get(name="testuser")
         self.assertFalse(u.verify_passwd("testpass"))
+
+    def test_add_group(self):
+        _add_test_user("testuser1")
+        g = groups.Group(
+            name="testgroup", members=["testuser1"]
+        )
+        g.add()
+
+    def test_upd_group(self):
+        _add_test_user("testuser1")
+        _add_test_user("testuser2")
+        g = groups.Group(
+            name="testgroup", members=["testuser1"]
+        )
+        g.add()
+        g = groups.get(name="testgroup")
+        g.members = ["testuser2"]
+        g.update()
+        g = groups.get(name="testgroup")
+        self.assertEqual(g.members, ["testuser2"])
+
+    def test_del_group(self):
+        g = groups.Group(name="testgroup", members=[])
+        g.add()
+        g = groups.get(name="testgroup")
+        g.delete()
+        self.assertIsNone(groups.get(name="testgroup"))
+
+    def test_add_domain(self):
+        d = domains.Domain("testdomain.xyz")
+        d.add()
+        self.assertIsNotNone(domains.get("testdomain.xyz"))
+
+    def test_del_domain(self):
+        d = domains.Domain("testdomain.xyz")
+        d.add()
+        d = domains.get("testdomain.xyz")
+        d.remove()
+        self.assertIsNone(domains.get("testdomain.xyz"))
+
+
+def _add_test_user(uname):
+    u = users.User(
+        name=uname, first_name="Test", last_name="User",
+        domain="localhost", admin=True, sudo=False
+    )
+    u.add(passwd="testpass")
